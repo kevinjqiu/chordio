fdb6e80e75a4977e7df5454f0d734e3a8d2e5275
Kevin Jing Qiu <kevin@idempotent.ca>
2020-05-04 01:41:25 -0400

    Remove Node struct

--- node.go
+++ node.go
@@ -17,28 +17,6 @@ type NodeRef struct {
 	bind string
 }
 
-type Node struct {
-	id   ChordID
-	bind string
-	pred ChordID
-	succ ChordID
-}
-
-func (n Node) GetID() ChordID   { return n.id }
-func (n Node) GetBind() string  { return n.bind }
-func (n Node) GetPred() ChordID { return n.pred }
-func (n Node) GetSucc() ChordID { return n.succ }
-
-func newNode(bind string, m Rank) Node {
-	n := Node{
-		bind: bind,
-	}
-	n.id = assignID([]byte(bind), m)
-	n.pred = n.id
-	n.succ = n.id
-	return n
-}
-
 func assignID(key []byte, m Rank) ChordID {
 	hasher := sha1.New()
 	hasher.Write(key)
